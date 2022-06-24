68d2af94d5926c3d57cec51d6cc6a1cbb942f83b
jhh <ejy0416@naver.com>
2022-06-24 18:14:51 +0900

    공지사항 상세보기

--- src/main/java/com/nike/web/domain/OrderDetailDTO.java
+++ src/main/java/com/nike/web/domain/OrderDetailDTO.java
@@ -5,32 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-<<<<<<< HEAD
-@Data
-@NoArgsConstructor
-=======
-
->>>>>>> 42cbe105611f332659269d2db15c72e066893d3f
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class OrderDetailDTO {
 
-<<<<<<< HEAD
-
-	private int odNo;
-	private int oNo;
-	private int pNo;
-	private int oQty;
-	private int oPrice;
-
-}
-
-
-
-
-=======
 	private int orderDetailNo;
 	private int orderNo;
 	private int productNo;
@@ -38,4 +18,3 @@ public class OrderDetailDTO {
 	private int orderPrice;
 
 }
->>>>>>> 42cbe105611f332659269d2db15c72e066893d3f
--- src/main/java/com/nike/web/service/OrderServiceImpl.java
+++ src/main/java/com/nike/web/service/OrderServiceImpl.java
@@ -5,4 +5,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl implements OrderService {
 
+	@Override
+	public String getTokens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int paymentInfo(String impUid, String token) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void paymentCancle(String token, String impUid, int orderAmount, String reason) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
