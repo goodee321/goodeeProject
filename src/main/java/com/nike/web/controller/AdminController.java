5994fdd66b8af4eef70f591ade6dee3eaa7035a9
jhh <ejy0416@naver.com>
2022-07-04 15:12:29 +0900

    order 커밋

--- src/main/java/com/nike/web/controller/AdminController.java
+++ src/main/java/com/nike/web/controller/AdminController.java
@@ -1,16 +1,31 @@
 package com.nike.web.controller;
 
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.nike.web.domain.MemberDTO;
 import com.nike.web.service.MemberService;
 import com.nike.web.service.NoticeService;
+import com.nike.web.service.OrderService;
+import com.nike.web.service.ProductService;
+import com.nike.web.service.QnaService;
 
 @Controller
 public class AdminController {
@@ -21,31 +36,54 @@ public class AdminController {
 	@Autowired
 	private MemberService memberService;
 	
-		@Autowired
-		private NoticeService noticeService;
+	@Autowired
+	private NoticeService noticeService;
+		
+	@Autowired
+	private ProductService productService;
+	
+	@Autowired
+	private OrderService orderService;
+	
+	@Autowired
+	private QnaService qnaService;
+	
+	
+//	
+//	// 메인
+//	@GetMapping("/admin/main")
+//	public String mainPage() {
+//		return "admin/main";
+//	}
 	
 	
 	
-	@GetMapping("/admin/main")
-	public String mainPage() {
-		return "admin/main";
+	// 회원목록
+	@GetMapping("/admin/member/list")
+	public String list(HttpServletRequest request, Model model) {
+		memberService.getMembers(request, model);
+		return "admin/member/list";	// search.jsp를 열면 list.jsp가 포함되어 있으므로 search.jsp로 간다.
 	}
 	
 	
-	// 회원관리
-	@GetMapping("/admin/member/list")
-	public String list(HttpServletRequest request, Model model) {
-		memberService.findMembers(request, model);
+	//
+	@GetMapping("/admin/member/searchPage")
+	public String searchPage() {
 		return "admin/member/list";
 	}
 	
+	//
+	@GetMapping("/admin/member/search")
+	public String search(HttpServletRequest request, Model model) {
+		memberService.findMembers(request, model);
+		return "admin/member/list";	
+	}
 	
-	// 회원개별삭제
-	@GetMapping("/admin/member/removeOne")
-	public String removeOne(HttpServletRequest request, RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute("kind", "deleteOne");
-		redirectAttributes.addFlashAttribute("res", memberService.removeOne(request));
-		return "redirect:/admin/member/afterDML";
+	
+	// result 맵핑
+	@GetMapping("/admin/member/afterDML")
+	public String afterDML() {
+		return "admin/member/result";   // admin 폴더 아래 member 폴더 아래 result.jsp로 이동
 	}
 	
 	
@@ -58,11 +96,14 @@ public class AdminController {
 	}
 	
 	
-	// 
-	@GetMapping("/admin/member/afterDML")
-	public String afterDML() {
-		return "admin/member/result";   // admin 폴더 아래 member 폴더 아래 result.jsp로 이동
+	// 회원개별삭제
+	@GetMapping("/admin/member/removeOne")
+	public String removeOne(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("kind", "deleteOne");
+		redirectAttributes.addFlashAttribute("res", memberService.removeOne(request));
+		return "redirect:/admin/member/afterDML";
 	}
+	
 	
 	// 회원상세보기
 	@GetMapping("/admin/member/detail")
@@ -70,6 +111,7 @@ public class AdminController {
 		model.addAttribute("member", memberService.findMemberByNo(request));
 		return "admin/member/detail";  // admin 폴더 아래 member 폴더 아래 detail.jsp로 이동
 	}
+	
 	
 	// 회원수정
 	@GetMapping("/admin/member/changePage")
@@ -79,7 +121,7 @@ public class AdminController {
 	}
 	
 	
-	// 여기까지 회원관리
+	// 
 	@PostMapping("/admin/member/change")
 	public String change(HttpServletRequest request, RedirectAttributes redirectAttributes) {
 		redirectAttributes.addFlashAttribute("kind", "update");
@@ -89,30 +131,428 @@ public class AdminController {
 	
 	
 	
-		// 공지사항관리
-		@GetMapping("/admin/notice/list")
-		public String noticeList(HttpServletRequest request, Model model) {
-			noticeService.findNotices2(request, model);
-			return "admin/notice/list";
-		}
-		
-		
-		// 공지상세보기
-		@GetMapping("/admin/notice/detail")
-		public String noticeDetail(HttpServletRequest request, Model model) {
-			model.addAttribute("notice", noticeService.findNoticeByNo2(request));
-			return "admin/notice/detail";  // admin 폴더 아래 member 폴더 아래 detail.jsp로 이동
-		}
-		
-		
-		
-		
 	
 	
 	
+				// 공지사항목록
+				@GetMapping("/admin/notice/list")
+				public String noticeList(HttpServletRequest request, Model model) {
+					noticeService.getNotices2(request, model);
+					return "admin/notice/list";
+				}
+				
+				
+				// 
+				@GetMapping("/admin/notice/searchPage")
+				public String noticeSearchPage() {
+					return "admin/notice/list";
+				}
+				
+				
+				// 
+				@GetMapping("/admin/notice/search")
+				public String noticeSearch(HttpServletRequest request, Model model) {
+					noticeService.findNotices2(request, model);
+					return "admin/notice/list";
+				}
+				
+				
+				
+				
+				
+				
+				// 공지상세보기
+				@GetMapping("/admin/notice/detail")
+				public String noticeDetail(HttpServletRequest request, Model model) {
+					model.addAttribute("notice", noticeService.findNoticeByNo2(request));
+					return "admin/notice/detail";  // admin 폴더 아래 member 폴더 아래 detail.jsp로 이동
+				}
+				
+				
+				// 공지사항수정
+				@GetMapping("/admin/notice/changePage")
+				public String noticeChangePage(HttpServletRequest request, Model model) {
+					model.addAttribute("notice", noticeService.findNoticeByNo2(request));
+					return "admin/notice/change";  // admin 폴더 아래 member 폴더 아래 change.jsp로 이동
+				}
+				
+				
+				// 공지사항수정
+				@PostMapping("/admin/notice/change")
+				public String noticeChange(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+					redirectAttributes.addFlashAttribute("kind", "update");
+					redirectAttributes.addFlashAttribute("res", noticeService.change2(request));
+					return "redirect:/admin/notice/afterDML";
+				}
+				
+			
+				// result 맵핑
+				@GetMapping("/admin/notice/afterDML")
+				public String NoticeafterDML() {
+					return "admin/notice/result";   // admin 폴더 아래 member 폴더 아래 result.jsp로 이동
+				}
+			
+			
+				
+				// 공지사항개별삭제
+				@GetMapping("/admin/notice/removeOne")
+				public String noticeRemoveOne(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+					redirectAttributes.addFlashAttribute("kind", "deleteOne");
+					redirectAttributes.addFlashAttribute("res", noticeService.removeOne(request));
+					return "redirect:/admin/notice/afterDML";
+				}
+				
+				
+				
+				// 공지사항삽입
+				@GetMapping("/admin/notice/savePage")
+				public String noticeSavePage() {
+					return "admin/notice/save";
+				}
+				
+				
+				// 
+				@PostMapping("/admin/notice/save")
+				public String noticeSave(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+					redirectAttributes.addFlashAttribute("insRes", noticeService.save2(request));
+					redirectAttributes.addFlashAttribute("type", "insert");
+					return "redirect:/admin/notice/afterDML";  
+				}
 	
-	
+		
+				// 공지사항선택삭제
+				@GetMapping("/admin/notice/removeList")
+				public String noticeRemoveList(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+					redirectAttributes.addFlashAttribute("kind", "deleteList");
+					redirectAttributes.addFlashAttribute("res", noticeService.removeList(request));
+					return "redirect:/admin/notice/afterDML";
+				}
 
+				
+	
+		
+	
+	// 상품 목록
+	@GetMapping("/admin/product/list")
+	public String productList(HttpServletRequest request, Model model) {
+		productService.findProducts2(request, model);
+		return "/admin/product/list";
+	}
+	
+	// 썸네일 보여주기
+	@ResponseBody
+	@GetMapping("/admin/product/display")
+	public ResponseEntity<byte[]> display(@RequestParam(value="proimgNo", required=true) Integer ProimgNo, @RequestParam(value="type", required=false, defaultValue="image") String type) {
+		return productService.display(ProimgNo, type);		
+	}
+	
+	
+	
+	// 검색기능
+	@GetMapping("/admin/product/find")
+	public String find(HttpServletRequest request, Model model) {
+		productService.getFindProducts2(request, model);
+		return "/admin/product/list";
+	}
+	
+	
+	// 삽입
+	@GetMapping("/admin/product/saveProductPage")
+	public String saveProductPage() {
+		return "/admin/product/saveProduct";
+	}
+	
+	// 
+	@PostMapping("/admin/product/saveProduct")
+	public void saveProduct(MultipartHttpServletRequest multipartRequest, HttpServletResponse response, Model model) {
+		productService.save2(multipartRequest, response, model);
+	}
+	
+	
+	// 상품선택삭제
+	@GetMapping("/admin/product/removeList")
+	public String ProductRemoveList(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("kind", "deleteList");
+		redirectAttributes.addFlashAttribute("res", productService.removeList(request));
+		return "redirect:/admin/product/afterDML";
+	}
+	
+	
+	// result 맵핑
+	@GetMapping("/admin/product/afterDML")
+	public String productAfterDML() {
+		return "admin/product/result";   // admin 폴더 아래 member 폴더 아래 result.jsp로 이동
+	}
+	
+	
+	
+	// 상품 상세보기
+	@GetMapping("/admin/product/detail")
+	public String productDetail(HttpServletRequest request, Model model) {
+		model.addAttribute("product", productService.findProductByNo2(request));
+		return "admin/product/detail";  // admin 폴더 아래 member 폴더 아래 detail.jsp로 이동
+	}
+		
+		
+		
+	// 제품 수정
+	@GetMapping("/admin/product/changeProductPage")
+	public String changeProductPage(HttpServletRequest request, Model model) {
+		productService.changeProductOptionPage(request, model);
+		return "admin/product/changeProduct";
+	}
+		
+		
+		
+		// 
+		@PostMapping("/admin/product/changeProduct")
+		public void changeProduct(MultipartHttpServletRequest multipartRequest, HttpServletResponse response) {
+			productService.changeProduct2(multipartRequest, response);
+		}
+		
+		
+		
+		
+		
+		// 상품 옵션 수정
+		@GetMapping("/admin/product/changeProductOptionPage")
+		public String changeProductOptionPage(@RequestParam Integer proNo,HttpServletRequest request, Model model) {
+			model.addAttribute("product", productService.getProductByNo(proNo));
+			return "admin/product/changeProductOption";
+		}
+		
+		
+		// 
+		@PostMapping("/admin/product/changeProductOption")
+		public void changeProductOption(HttpServletRequest request, HttpServletResponse response) {
+				productService.changeProductOption2(request, response);
+		}
+		
+		
+		
+		// 상품삭제
+		@GetMapping("/admin/product/productDelete")
+		public void productDelete(HttpServletRequest request, HttpServletResponse response) {
+				productService.productDelete2(request,response);
+		}
+		
+		
+		
+		
+		// 옵션추가
+		@GetMapping("/admin/product/saveProductOption")
+		public String saveProductOption(@RequestParam Integer proNo, HttpServletRequest request, Model model) {
+			model.addAttribute("product", productService.getProductByNo2(proNo));
+			return "admin/product/saveProductOption";
+			
+		}
+
+		// 
+		@RequestMapping(value="/admin/product/saveProductOptionOk", method= {RequestMethod.GET, RequestMethod.POST})
+		public void saveProductOptionOk(HttpServletRequest request, HttpServletResponse response) {
+			productService.saveProductOptionOk2(request, response);
+		}
+		
+		
+		
+		// [주문]
+		// 주문목록
+		@GetMapping("/admin/order/list")
+		public String orderList(HttpServletRequest request, Model model) {
+			orderService.getOrders2(request, model);
+			return "admin/order/list";	// search.jsp를 열면 list.jsp가 포함되어 있으므로 search.jsp로 간다.
+		}
+		
+		
+		//
+		@GetMapping("/admin/order/searchPage")
+		public String orderSearchPage() {
+			return "admin/order/list";
+		}
+		
+		
+		//
+		@GetMapping("/admin/order/search")
+		public String orderSearch(HttpServletRequest request, Model model) {
+			orderService.findOrders2(request, model);
+			return "admin/order/list";	
+		}
+		
+		
+		// 주문상세보기
+		@GetMapping("/admin/order/detail")
+		public String orderDetail(HttpServletRequest request, Model model) {
+			model.addAttribute("order", orderService.findOrderByNo2(request));
+			return "admin/order/detail";  // admin 폴더 아래 member 폴더 아래 detail.jsp로 이동
+		}
+		
+		
+		// result 맵핑
+		@GetMapping("/admin/order/afterDML")
+		public String orderAfterDML() {
+			return "admin/order/result";   // admin 폴더 아래 member 폴더 아래 result.jsp로 이동
+		}
+		
+		
+		
+		// 주문수정
+		@GetMapping("/admin/order/changePage")
+		public String orderChangePage(HttpServletRequest request, Model model) {
+			model.addAttribute("order", orderService.findOrderByNo2(request));
+			return "admin/order/change";  // admin 폴더 아래 member 폴더 아래 change.jsp로 이동
+		}
+		
+		
+		// 
+		@PostMapping("/admin/order/change")
+		public String orderChange(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+			redirectAttributes.addFlashAttribute("kind", "update");
+			redirectAttributes.addFlashAttribute("res", orderService.change2(request));
+			return "redirect:/admin/order/afterDML";
+		}
+		
+		
+		
+		// 주문선택삭제
+		@GetMapping("/admin/order/removeList")
+		public String orderRemoveList(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+			redirectAttributes.addFlashAttribute("kind", "deleteList");
+			redirectAttributes.addFlashAttribute("res", orderService.removeList2(request));
+			return "redirect:/admin/order/afterDML";
+		}
+		
+		
+		// 주문개별삭제
+		@GetMapping("/admin/order/removeOne")
+		public String orderRemoveOne(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+			redirectAttributes.addFlashAttribute("kind", "deleteOne");
+			redirectAttributes.addFlashAttribute("res", orderService.removeOne2(request));
+			return "redirect:/admin/order/afterDML";
+		}
+		
+		
+		
+		
+		// [qna]
+		@GetMapping("/admin/qna/list")
+		public String qnaList(HttpServletRequest request, Model model) {
+			qnaService.findQnas2(request, model);
+			return "admin/qna/list"; 
+		}
+		
+		
+		// qna
+		@GetMapping("/admin/qna/detail")
+		public String qnaDetail(HttpServletRequest request, HttpServletResponse response, Model model) {
+			qnaService.findQnaByNo2(request, model);
+			return "admin/qna/detail";
+		}
+		
+		
+		// qna
+		@GetMapping("/admin/qna/changePage")
+		public String qnaChangePage(HttpServletRequest request, Model model) {
+			qnaService.findQnaByNo2(request, model);
+			return "admin/qna/change";
+		}
+		
+		
+		// qna
+		@PostMapping("/admin/qna/change")
+		public void qnaChange(HttpServletRequest request, HttpServletResponse response) {
+			qnaService.change2(request, response);
+		}
+		
+		
+		// qna
+		@GetMapping("/admin/qna/remove")
+		public void qnaRemove(HttpServletRequest request, HttpServletResponse response){
+			int qnaNo = Integer.parseInt(request.getParameter("qnaNo"));
+			int res = qnaService.removeQna2(qnaNo);
+			try {
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				if(res > 0) {
+					out.println("<script>");
+					out.println("alert('게시글이 삭제되었습니다.')");
+					out.println("location.href='" + request.getContextPath() + "/admin/qna/list'");
+					out.println("</script>");
+					out.close();
+				} else {
+					out.println("<script>");
+					out.println("alert('게시글이 삭제되지 않았습니다.')");
+					out.println("history.back()");
+					out.println("</script>");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+				
+		}
+		
+		
+		
+		//
+		@GetMapping("/admin/qna/saveQna")
+		public String saveQna2() {
+			return "admin/qna/save";
+		}
+		
+		
+		//
+		@PostMapping("/admin/qna/save")
+		public void qnaSave(HttpServletRequest request, HttpServletResponse response) {
+			int res = qnaService.saveQna2(request);
+			try {
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				if(res > 0) {
+					out.println("<script>");
+					out.println("alert('게시글이 등록되었습니다.')");
+					out.println("location.href='" + request.getContextPath() + "/admin/qna/list'");
+					out.println("</script>");
+					out.close();
+				} else {
+					out.println("<script>");
+					out.println("alert('게시글이 등록되지 않았습니다.')");
+					out.println("history.back()");
+					out.println("</script>");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		//
+		@PostMapping("/admin/qna/saveReply")
+		public void saveReply(HttpServletRequest request, HttpServletResponse response) {
+			int res = qnaService.saveReply2(request);
+			try {
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				if(res > 0) {
+					out.println("<script>");
+					out.println("alert('답변이 등록되었습니다.')");
+					out.println("location.href='" + request.getContextPath() + "/admin/qna/list'");
+					out.println("</script>");
+					out.close();
+				} else {
+					out.println("<script>");
+					out.println("alert('답변이 등록되지 않았습니다.')");
+					out.println("history.back()");
+					out.println("</script>");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	
+	
+	
 	
 	
 	
---
+++ src/main/webapp/WEB-INF/views/admin/order/list.jsp
@@ -0,0 +1,361 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+
+<!-- bootstrap css -->
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath}/resources/css/bootstrap.min.css">
+	
+<style>
+
+	@import url('https://fonts.googleapis.com/css2?family=Splash&display=swap');
+
+	.blind{display: none; }
+
+	.unlink, .link {
+		display: inline-block;  /* 같은 줄에 둘 수 있고, width, height 등 크기 지정 속성을 지정할 수 있다. */
+		padding: 10px;
+		margin: 5px;
+		border: 1px solid white;
+		text-align: center;
+		text-decoration: none;  /* 링크 밑줄 없애기 */
+		color: gray;
+		font-weight: bold;
+	}
+	.link:hover {
+		border: 1px solid orange;
+		color: limegreen;
+	}
+
+
+
+	table {
+		border-collapse: collapse;
+		margin-left:auto;
+    	margin-right:auto;
+	}
+	
+	td:nth-of-type(1) { width: 80px; }
+	td:nth-of-type(2) { width: 160px; }
+	td:nth-of-type(3) { width: 240px; }
+	td:nth-of-type(4) { width: 240px; }
+	td:nth-of-type(5) { width: 120px; }
+	td:nth-of-type(6) { width: 160px; }
+	td:nth-of-type(7) { width: 160px; }
+	
+	td {
+		padding: 5px;
+		text-align: center;
+	}
+	tfoot td {
+		border-left: 0;
+		border-right: 0;
+		border-bottom: 0;
+	}
+	
+	input[type="checkbox"] {
+  		width: 27px; 
+  		height: 27px; 
+	}
+	
+	
+	thead {
+		font-weight: bold;
+	}
+	
+	
+	section {
+		background-color: #BDBDBD;
+		text-align: center;
+		font-family: Georgia, "Malgun Gothic", serif;
+	}
+	
+	
+	
+	.kind {
+			font-family: 'Splash', cursive;
+			font-size: 40px;
+	}
+	
+	.kind2 {
+			font-family: 'Splash', cursive;
+			font-size: 27px;
+	}
+	
+</style>
+
+
+
+<script src="../../resources/js/jquery-3.6.0.js"></script>
+<script>
+
+	
+	
+	
+	
+	
+	
+	
+
+
+	$(document).ready(function(){
+		
+		$('tbody td:not(td:first-of-type)').on('click', function(){
+			var orderNo = $(this).parent().data('order_no');
+			location.href='${contextPath}/admin/order/detail?orderNo=' + orderNo;
+		})
+		
+		
+		
+		/*
+		$('tbody tr').on('click', function(){
+			// $(this)                          : 클릭한 행을 의미한다.
+			//                                    <tr>...</tr>
+			// $(this).find('.noticeNo')        : 클릭한 행 내부에 있는 class="noticeNo" 요소를 의미한다.
+			//                                    <td class="noticeNo">1</td>
+			// $(this).find('.noticeNo').text() : 클릭한 행 내부에 있는 class="noticeNo" 요소의 텍스트를 의미한다.
+			//                                    1
+			var noticeNo = $(this).find('.noticeNo').text();
+			location.href='${contextPath}/notice/detail?noticeNo=' + noticeNo;
+		})
+		*/
+		
+		
+		// 전체 선택 클릭하기
+		// 전체 선택을 체크하면 개별 선택도 모두 체크
+		// 전체 선택을 해제하면 개별 선택도 모두 해제
+		var checkAll = $('#checkAll');
+		var checkes = $('.checkes');
+		checkAll.on('click', function(){
+			for(let i = 0; i < checkes.length; i++) {
+				// check[i].prop('checked', true);  // 전체 선택이 체크된 경우
+				// check[i].prop('checked', false); // 전체 선택이 해제된 경우
+				$(checkes[i]).prop('checked', checkAll.prop('checked'));
+			}
+			$.each(checkes, function(i, check){
+				$(check).prop('checked', checkAll.prop('checked'));
+			})
+		})
+		
+		// 개별 선택 클릭하기
+		// 개별 선택을 클릭하면
+		// 모든 개별 선택을 확인해서
+		// 모두 체크되어 있으면 전체 선택 체크하고,
+		// 하나라도 체크 해제되어 있으면 전체 선택 해제한다.
+		for(let i = 0; i < checkes.length; i++){
+			$(checkes[i]).on('click', function(){
+				for(let j = 0; j < checkes.length; j++){
+					if($(checkes[j]).prop('checked') == false){  // 하나라도 체크 해제되었다면,
+						checkAll.prop('checked', false);         // 전체 선택 해제하고,
+						return;                                  // 함수 종료(클릭 이벤트 리스너)
+					}
+				}
+				checkAll.prop('checked', true);             // 체크 해제된 것이 하나도 발견되지 않은 경우 전체 선택 체크
+			})
+		}
+		
+	})
+	
+	
+	
+	$(function(){
+	fnAreaChoice();
+	fnSearchAll();
+	fnSearch();
+	})
+	
+	
+	
+	
+	
+	// 함수
+	function fnAreaChoice(){
+		
+		// 초기
+		$('#equalArea').css('display', 'none');
+	
+		// 선택
+		$('#column').on('change', function(){
+			if( $(this).val() == '' ) {
+				$('#equalArea').css('display', 'none');
+			} else if( $(this).val() == 'ORDER_NO' || $(this).val() == 'MEMBER_NO' ) {
+				$('#equalArea').css('display', 'inline');
+			} else {
+				$('#equalArea').css('display', 'none');
+			}
+		})
+		
+	}
+	
+	function fnSearchAll(){
+		$('#btnSearchAll').on('click', function(){
+			location.href="${contextPath}/admin/order/list";
+		})
+	}
+	
+	function fnSearch(){
+		
+		var column = $('#column');
+		var query = $('#query');
+		
+		
+		$('#btnSearch').on('click', function(){
+			
+			
+			// 사원번호 검색
+			// var regId = /^[0-9]{3}$/;  // 숫자 3자리여야 함.
+			// if( column.val() == 'ID' && regId.test(query.val()) == false) {
+				// alert('사원번호가 올바르지 않습니다.');
+				// query.focus();
+				// return;
+			// }
+			
+			
+			
+			// 검색 실행
+			// equalArea 작업은 column, query 파라미터 전송
+			if( query.val() == '') {
+				alert('검색어를 입력해주세요.');
+				event.preventDefault();
+				return false;
+			}
+			
+			else if( column.val() == 'ORDER_NO' || column.val() == 'MEMBER_NO' ) {
+				location.href="${contextPath}/admin/order/search?column=" + column.val() + "&query=" + query.val();
+			} 
+			
+		})
+		
+		
+	}
+	
+	
+	
+	
+	
+</script>
+	
+</head>
+
+
+<body>
+
+	<nav id="nav">
+		<div id="nav_box">
+			<%@ include file="../../layout/adminNav.jsp" %>
+		</div>
+		
+	</nav>
+	
+
+
+<section>
+	<br>
+	
+	<div>
+	<h3 class="kind">Order Management</h3>
+	
+	<h3 class="kind2">Total Orders: ${totalRecord}</h3>
+	
+	<h3 class="kind2">Search Orders: ${findRecord}</h3>
+	</div>
+	
+	
+	
+	
+	<hr>
+	
+	
+	<form id="f" method="get">
+	
+		<select name="column" id="column">
+			<option value="">:::선택:::</option>
+			<option value="ORDER_NO">주문번호</option>
+			<option value="MEMBER_NO">회원번호</option>
+		</select>
+		
+		<span id="equalArea">
+			
+			<input type="text" name="query" id="query">
+			
+		</span>
+		
+		<input type="button" value="검색" id="btnSearch" class="btn btn-secondary" >
+		<input type="button" value="전체주문조회" id="btnSearchAll" class="btn btn-secondary">
+		
+	</form>
+	
+	
+	
+	<form id="f2" action="${contextPath}/admin/order/removeList">
+
+		
+	
+		<table id="boardtable"  class="table-hover"><br>
+		
+			<thead>
+				<tr>
+					<td class="table-dark" scope="col">
+						<label for="checkAll">전체선택</label>
+						<input type="checkbox" id="checkAll" class="blind">
+					</td>
+					<td class="table-dark">주문번호</td>
+					<td class="table-dark">회원번호</td>
+					<td class="table-dark">회원명</td>
+					<td class="table-dark">주문일</td>
+					
+				</tr>
+			</thead>
+			
+			<tbody class="table-secondary">
+				<c:forEach items="${orders}" var="order">
+					<tr data-order_no="${order.orderNo}">
+						<td><input type="checkbox" name="orderNoList" id="orderNoList" value="${order.orderNo}" class="checkes"></td>
+						<td>${order.orderNo}</td>
+						<td>${order.memberNo}</td>
+						<td>${order.orderName}</td>
+						<td>${order.orderDate}</td>
+						
+					</tr>
+				</c:forEach>
+			</tbody>
+			
+			<tfoot>
+			<tr>
+				 <td colspan="5">
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					${paging}
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					<button id="btnRemove" class="btn btn-secondary">선택삭제</button><br><br>
+				</td>
+				
+				
+			</tr>
+		</tfoot>
+		
+		</table>
+	</form><br>
+	
+	
+	
+	
+</section>
+
+<footer id="footer">
+	<div id="footer_box">
+		<%@ include file="../../layout/adminFooter.jsp" %>
+	</div>
+</footer>
+
+</body>
+</html>
